d384f16731e99a2a31d0524578efb13911b0144a
neobis2022team2autnm <project.autumn.2022.team2@gmail.com>
2023-01-03 16:54:11 +0600

    init : done request for backend and get all pizzas

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
@@ -6,18 +7,32 @@ import pizzas from './assets/pizzas.json';
 import './scss/app.scss';
 
 function App() {
+  //https://61bdf7c82a1dd4001708a1cf.mockapi.io/items
+
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://61bdf7c82a1dd4001708a1cf.mockapi.io/items')
+      .then((res) => {
+        return res.json();
+      })
+      .then((arr) => {
+        setItems(arr);
+      });
+  }, []);
+  console.log(items);
   return (
     <div class="wrapper">
       <Header />
       <div class="content">
         <div class="container">
           <div class="content__top">
-            {/* <Categories /> */}
+            <Categories />
             <Sort />
           </div>
           <h2 class="content__title">Все пиццы</h2>
           <div class="content__items">
-            {pizzas.map((obj) => (
+            {items.map((obj) => (
               <PizzaBlock key={obj.id} {...obj} />
             ))}
           </div>
